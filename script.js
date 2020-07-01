be06f3f8ff9296d28d9204ef6bc54648697e8ba9
unknown <asotjrs@gmail.com>
2020-07-02 00:57:47 +0200

    adding a basic script search

---
+++ script.js
@@ -0,0 +1,22 @@
+const googleDB=[
+  "cats.com",
+  "bikies.com",
+  "cars.com",
+  "cutecats.com",
+  "beautifulcats.com",
+   "catsmiaw.com",
+   "catkitsmiaw.com",
+   "animals.com",
+
+];
+
+
+const res=(inp)=>{
+
+    const filtered=googleDB.filter(link=>{
+        return link.includes(inp);
+    });
+    return filtered.length>3 ? filtered.slice(0,3):filtered;
+
+};
+console.log(res("cat"));
